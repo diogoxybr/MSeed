536a9d894e84ac754a32884cf9867a4a01c1c5cc
diogoxybr <diogoxy@gmail.com>
2022-05-06 00:26:32 -0300

    AuthJWT

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 //Caso seja e primeira vez rode: npm install express nodemon mongoose
-//Para rodar use: npm ini -y & npm
+//Para rodar use: npm init -y & npm run start
 
 //config inicial
+// imports
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const { add } = require('nodemon/lib/rules')
 const app = express()
 
@@ -15,12 +18,13 @@ app.use(
     }),
 )
 
+//Config JSON response
 app.use(express.json())
 
-// rotas da API
-const personRoutes = require('./routes/personRoutes')
-app.use('/person', personRoutes)
+// Models
+const User = require('./models/User')
 
+// rotas da API
 const commentRoutes = require('./routes/commentRoutes')
 app.use('/comment', commentRoutes)
 
@@ -30,12 +34,157 @@ app.use('/monitoring', monitoringRoutes)
 const plantRoutes = require('./routes/plantRoutes.js')
 app.use('/plant', plantRoutes)
 
-//rota inicial / endpoint
+//Rota inicial - Endpoint
+//Open Route - Public Route
 app.get('/', (req, res) => {
     // mostrar req
-    res.json({ message: 'Oi Express!' })
+    res.status(200).json({ message: 'Bem vindo à API do Magic Seed' })
 })
 
+// Private Route
+app.get("/user/:id", checkToken, async (req, res) => {
+
+    const id = req.params.id
+
+    // check if user exists
+    // Verifica se o usuário já existe
+    const user = await User.findById(id, '-password')
+
+    if (!user) {
+        return res.status(404).json({ msg: 'Usuário não encontrado!' })
+    }
+
+    res.status(200).json({ user })
+})
+
+function checkToken(req, res, next) {
+
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).json({ msg: 'Acesso negado!' })
+    }
+
+    try {
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+
+    } catch (error) {
+
+        res.status(400).json({ msg: "Token inválido!" })
+
+    }
+}
+
+//Registrar usuário
+app.post('/auth/register', async (req, res) => {
+
+    const { name, email, password, confirmpassword } = req.body
+
+    //validações
+    if (!name) {
+        return res.status(422).json({ msg: 'O nome é obrigatorio!' })
+    }
+
+    if (!email) {
+        return res.status(422).json({ msg: 'O email é obrigatorio!' })
+    }
+
+    if (!password) {
+        return res.status(422).json({ msg: 'A senha é obrigatoria!' })
+    }
+
+    if (password !== confirmpassword) {
+        return res.status(422).json({ msg: 'As senhas não conferem!' })
+    }
+
+    // check if user exists
+    // Verifica se o usuário já existe
+    const userExists = await User.findOne({ email: email })
+
+    if (userExists) {
+        return res.status(422).json({ msg: 'Por favor, utilize outro e-mail!' })
+    }
+
+    // create password
+    // criar senha
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    //create user
+    //criar usuário
+    const user = new User({
+        name,
+        email,
+        password: passwordHash,
+    })
+
+    try {
+        await user.save()
+
+        res.status(201).json({ msg: 'Usuário criado com sucesso!' })
+    } catch (error) {
+        console.log(error)
+
+        res.status(500).json({ msg: 'Aconteceu um erro no servidor, tente mais tarde' })
+    }
+})
+
+// Login User
+// Authenticação de usuário
+app.post("/auth/login", async (req, res) => {
+    const { email, password } = req.body
+
+    //validations
+    if (!email) {
+        return res.status(422).json({ msg: 'O email é obrigatorio!' })
+    }
+
+    if (!password) {
+        return res.status(422).json({ msg: 'A senha é obrigatoria!' })
+    }
+
+    // check if user exists
+    // Verifica se o usuário já existe
+    const user = await User.findOne({ email: email })
+
+    if (!user) {
+        return res.status(422).json({ msg: 'Usuário não encontrado!' })
+    }
+
+    // check if password match
+    // verifica se a senha está correta
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if (!checkPassword) {
+        return res.status(422).json({ msg: 'Senha inválida!' })
+    }
+
+    try {
+
+        const secret = process.env.secret
+
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            secret,
+        )
+        res.status(200).json({ msg: "Autenticação realizada com sucesso", token })
+
+    } catch (error) {
+        console.log(error)
+
+        res.status(500).json({ msg: 'Aconteceu um erro no servidor, tente mais tarde' })
+    }
+
+})
+
+//Credenciais
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
 
--- models/person.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const Person = mongoose.model('Person', {
-    nome: String,
-    email: String,
-})
-module.exports = Person
